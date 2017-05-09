684ef837d4f65694c31c9271d5102fbcfbaa1f2e
Benjamin Kleiner <bizzl@users.sourceforge.net>
2017-05-09 17:33:51 +0200

    Small adaptations to web interface

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,19 +2,11 @@
 
 namespace AppBundle\Controller;
 
-use AppBundle\Entity\Url;
 use AppBundle\Service\UrlQuery;
-use Doctrine\Common\Annotations\AnnotationReader;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Mapping\Factory\ClassMetadataFactory;
-use Symfony\Component\Serializer\Mapping\Loader\AnnotationLoader;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
 
 class DefaultController extends Controller
 {
@@ -71,7 +63,7 @@ class DefaultController extends Controller
         if ($request->getContentType() == 'json') {
             return new JsonResponse($this->get('app.serializer')->json($variables, ['display']));
         } else {
-            return $this->render('default/list.html.twig', $variables);
+            return $this->render('default/list.html.twig', ['links' => $variables]);
         }
     }
 }
